0b29c7a33a494e84bbf184a0b074a29cb23f6029
Pedro Lucas Andrade <78179865+Pedrest15@users.noreply.github.com>
2023-04-16 12:53:36 -0300

    Update Striker_Year.py

--- pages/Striker_Year.py
+++ pages/Striker_Year.py
@@ -23,13 +23,13 @@ df = pd.read_csv('data/newTopGoalScorer.csv')
 if option_entity == 'chart':
     fig, ax = plt.subplots(facecolor='#0e1117')
 
-    ax.scatter(df.Year,df.Goals,color='#800080')
-    ax.plot(df.Year,df.Goals,color='#800080')
+    ax.scatter(df.Year,df.Goals.sort_values(),color='#800080')
+    ax.plot(df.Year,df.Goals.sort_values(),color='#800080')
     ax.set_title("Goals per Year",color='white')
 
     annotations = ['CR7','CR7','CR7','CR7','CR7','CR7','Messi','Lewan','Halland','Benzema']
 
-    for xi,yi,text in zip(df.Year,df.Goals,annotations):
+    for xi,yi,text in zip(df.Year,df.Goals.sort_values(),annotations):
         ax.annotate(text,
                     xy=(xi, yi), xycoords='data',
                     xytext=(1.5, 1.5), textcoords='offset points')
