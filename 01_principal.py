1584fd67a1090f6aff3e8222cf7be2dd540c375d
Pedro Lucas Andrade <78179865+Pedrest15@users.noreply.github.com>
2023-04-16 01:34:13 -0300

    Add files via upload

---
+++ 01_principal.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+st.title('Champions League Dataset')
+
+st.markdown('''
+Bla Bla Bla.
+''', unsafe_allow_html=True)
+
+st.info('Confira o projeto no [Github](https://github.com/Pedrest15/ChampionsLeagueDataset)')
